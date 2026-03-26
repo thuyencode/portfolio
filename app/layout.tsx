b7bee7aa5c210b819acf3a5f002759238003000e
Thuyen Code <thuyencode@proton.me>
2026-03-26 09:15:40 +0700

    Change ShadcnUI preset

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
-import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google"
+import { Geist, Geist_Mono, Nunito_Sans, Inter } from "next/font/google"
 import "./globals.css"
 
-const geistHeading = Geist({ subsets: ["latin"], variable: "--font-heading" })
+const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading'})
 
-const nunitoSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" })
+const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -21,8 +21,8 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
         "antialiased",
         fontMono.variable,
         "font-sans",
-        nunitoSans.variable,
-        geistHeading.variable
+        inter.variable,
+        nunitoSansHeading.variable
       )}
     >
       <body className="relative flex min-h-screen w-full flex-col items-center bg-white px-20 py-10">
