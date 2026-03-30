8dde68964e0ec4158f9fecb61635ea9670d66410
Thuyen Code <thuyencode@proton.me>
2026-03-30 17:06:49 +0700

    Temporaly remove option to set locale back to default

--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -2,16 +2,15 @@
 
 import { useLanguageSwitcher } from "@/hooks/use-language-switcher"
 import { Button, Dropdown } from "@heroui/react"
-import { useLocale, useTranslations } from "next-intl"
+import { Locale, useLocale, useTranslations } from "next-intl"
 
 export function LanguageSwitcher() {
-  const locale = useLocale()
   const t = useTranslations("component.LanguageSwitcher")
-  const switchToDefault = useLanguageSwitcher()
+  const locale = useLocale()
   const switchToEnglish = useLanguageSwitcher("en")
   const switchToVietnamese = useLanguageSwitcher("vi")
 
-  function getButtonLabel() {
+  function getButtonLabel(locale: Locale) {
     const flagEmoji =
       {
         en: "🇬🇧",
@@ -24,14 +23,11 @@ export function LanguageSwitcher() {
   return (
     <Dropdown>
       <Button variant="primary" className="">
-        {getButtonLabel()}
+        {getButtonLabel(locale)}
       </Button>
 
       <Dropdown.Popover className="min-w-fit" placement="bottom">
         <Dropdown.Menu>
-          <Dropdown.Item onClick={switchToDefault}>
-            {t("languageName.default")} 🌐
-          </Dropdown.Item>
           <Dropdown.Item onClick={switchToEnglish}>
             {t("languageName.english")} 🇬🇧
           </Dropdown.Item>
