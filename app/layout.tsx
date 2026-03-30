050570e61c55edc3c7cbaafdce646d655b5d7961
Thuyen Code <thuyencode@proton.me>
2026-03-30 16:08:16 +0700

    Change `body`'s height limit

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
         nunitoSansHeading.variable
       )}
     >
-      <body className="relative max-h-screen w-full p-10">
+      <body className="relative min-h-screen w-full p-10">
         <ThemeProvider>{children}</ThemeProvider>
         <Background />
       </body>
