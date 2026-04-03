e2bdc54e49576a569e9ede01730dfee6cd52619d
Thuyen Code <thuyencode@proton.me>
2026-04-03 10:09:55 +0700

    Update layout.tsx

--- app/[locale]/(top-menu)/layout.tsx
+++ app/[locale]/(top-menu)/layout.tsx
@@ -6,12 +6,12 @@ import { ButtonGroup } from "@heroui/react"
 export default function TopMenuLayout({ children }: LayoutProps<"/[locale]">) {
   return (
     <>
-      <div className="fixed inset-x-60 top-6 z-10 h-min">
+      <div className="fixed inset-x-0 top-6 z-10 h-min">
         <div className="liquid-glass absolute inset-x-0 mx-auto flex w-full max-w-xl justify-center rounded-3xl p-3">
           <NavLinks />
         </div>
 
-        <div className="absolute right-0 flex items-center rounded-3xl p-3">
+        <div className="absolute right-32 flex items-center rounded-3xl p-3">
           <ButtonGroup>
             <LocaleChanger />
             <ThemeChanger>
