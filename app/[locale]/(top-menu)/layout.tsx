ee8c22e4455549a18935c6567937f1e4d591a74f
Thuyen Code <thuyencode@proton.me>
2026-04-04 11:23:51 +0700

    Rename `liquid-glass` class to `glassmorphism`

--- app/[locale]/(top-menu)/layout.tsx
+++ app/[locale]/(top-menu)/layout.tsx
@@ -7,7 +7,7 @@ export default function TopMenuLayout({ children }: LayoutProps<"/[locale]">) {
   return (
     <>
       <div className="fixed inset-x-0 top-6 z-10 h-min">
-        <div className="liquid-glass absolute inset-x-0 mx-auto flex w-full max-w-xl justify-center rounded-3xl p-3">
+        <div className="glassmorphism absolute inset-x-0 mx-auto flex w-full max-w-xl justify-center rounded-3xl p-3">
           <NavLinks />
         </div>
 
