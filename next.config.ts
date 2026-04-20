a27d0812b8c37ff71193863e8bbf69be812d6cd7
Thuyen Code <thuyencode@proton.me>
2026-04-20 09:39:08 +0700

    Allow place holder image

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ import createNextIntlPlugin from "next-intl/plugin"
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+    ],
+  },
 }
 
 const withNextIntl = createNextIntlPlugin()
