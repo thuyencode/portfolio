d9deea46daf2b9f64e632a89a1bf4e57ededbb06
Thuyen Code <thuyencode@proton.me>
2026-05-05 16:39:41 +0700

    Create copy-button.tsx

---
+++ components/copy-button.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { Button, cn } from "@heroui/react"
+import { CheckIcon, CopyIcon } from "lucide-react"
+import { useState } from "react"
+
+type CopyButtonProps = Omit<React.ComponentProps<typeof Button>, "children"> & {
+  text: string
+}
+
+export function CopyButton({ text, className, ...props }: CopyButtonProps) {
+  const [copied, setCopied] = useState(false)
+  const [disabled] = useState(() => typeof navigator === "undefined")
+
+  function handleCopy() {
+    if (copied) {
+      return
+    }
+
+    navigator.clipboard.writeText(text).then(() => {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 2000)
+    })
+  }
+
+  return (
+    <Button
+      variant="tertiary"
+      isDisabled={disabled}
+      onClick={handleCopy}
+      className={cn(className, {
+        "bg-success text-success-foreground": copied,
+      })}
+      {...props}
+    >
+      {copied ? <CheckIcon /> : <CopyIcon />}
+      {text}
+    </Button>
+  )
+}
